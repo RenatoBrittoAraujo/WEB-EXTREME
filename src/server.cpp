8b34b5533a369ae713e3e044644eafc8c640afa8
RenatoBrittoAraujo <renatomwbbritto@gmail.com>
2020-04-02 21:34:13 -0300

    Remove useless includes

--- src/server.cpp
+++ src/server.cpp
@@ -1,11 +1,8 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <bits/stdc++.h>
 
 void handleRequest(int);
 
